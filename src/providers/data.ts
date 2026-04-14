eaf0701f0451eb774ef159cc6d903cd445f3ed7b
blueBozzy <hachipt2@gmail.com>
2026-04-14 18:46:45 -0400

    implement some fixes

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -2,6 +2,8 @@ import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import {BACKEND_BASE_URL} from "@/constants";
 import {ListResponse} from "@/types";
 
+if (!BACKEND_BASE_URL) throw new Error('BACKEND_BASE_URL is not configured')
+
 const options: CreateDataProviderOptions = {
   getList:{
     getEndpoint: ({ resource }) => {
@@ -28,13 +30,13 @@ const options: CreateDataProviderOptions = {
     },
 
     mapResponse: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
 
       return payload.data ?? [];
     },
 
     getTotalCount: async (response) =>{
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
     }
